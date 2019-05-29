11d0bec1f5841b04cb0dd5fb2c2415dc553c519f
migue99angel <posadasarraez@correo.ugr.es>
2019-05-29 16:51:58 +0200

    Añadiendo Practica 2

---
+++ Práctica 2/CMakeFiles/GLUILib.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/algebra3.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/algebra3.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/arcball.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/arcball.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_add_controls.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_add_controls.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_bitmap_img_data.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_bitmap_img_data.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_bitmaps.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_bitmaps.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_button.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_button.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_checkbox.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_checkbox.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_column.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_column.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_commandline.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_commandline.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_control.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_control.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_edittext.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_edittext.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_filebrowser.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_filebrowser.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_list.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_list.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_listbox.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_listbox.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_mouse_iaction.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_mouse_iaction.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_node.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_node.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_panel.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_panel.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_radio.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_radio.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_rollout.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_rollout.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_rotation.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_rotation.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_scrollbar.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_scrollbar.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_separator.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_separator.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_spinner.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_spinner.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_statictext.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_statictext.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_string.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_string.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_textbox.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_textbox.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_translation.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_translation.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_tree.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_tree.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_treepanel.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_treepanel.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/glui_window.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_window.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/quaternion.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/quaternion.cpp.o"
+  "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/src/glui/viewmodel.cpp" "/home/migue/Escritorio/IA/Practicas/Práctica 2/practica2/CMakeFiles/GLUILib.dir/src/glui/viewmodel.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Práctica 2/Comportamientos_Jugador/jugador.hpp
@@ -0,0 +1,80 @@
+#ifndef COMPORTAMIENTOJUGADOR_H
+#define COMPORTAMIENTOJUGADOR_H
+
+#include "comportamientos/comportamiento.hpp"
+
+#include <list>
+
+struct estado {
+  int fila;
+  int columna;
+  int orientacion;
+};
+
+struct nodo{
+	estado st;
+	list<Action> secuencia;
+	int coste;
+	bool operator<(const nodo& n) const{
+    if(coste != n.coste){
+      return coste > n.coste;
+    }
+    else
+      return secuencia.size() > n.secuencia.size();
+  }
+};
+
+class ComportamientoJugador : public Comportamiento {
+  public:
+    ComportamientoJugador(unsigned int size) : Comportamiento(size) {
+      // Inicializar Variables de Estado
+      fil = col = 99;
+      brujula = 0; // 0: Norte, 1:Este, 2:Sur, 3:Oeste
+      destino.fila = -1;
+      destino.columna = -1;
+      destino.orientacion = -1;
+      ultimaAccion = actIDLE;
+      estoy_bien_situado = false;
+      hayPlan = false;
+    }
+    ComportamientoJugador(std::vector< std::vector< unsigned char> > mapaR) : Comportamiento(mapaR) {
+      // Inicializar Variables de Estado
+      fil = col = 99;
+      brujula = 0; // 0: Norte, 1:Este, 2:Sur, 3:Oeste
+      destino.fila = -1;
+      destino.columna = -1;
+      destino.orientacion = -1;
+      ultimaAccion = actIDLE;
+      estoy_bien_situado = false;
+      hayPlan = false;
+    }
+    ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
+    ~ComportamientoJugador(){}
+
+    Action think(Sensores sensores);
+    int interact(Action accion, int valor);
+    void VisualizaPlan(const estado &st, const list<Action> &plan);
+    ComportamientoJugador * clone(){return new ComportamientoJugador(*this);
+    int getCosto(char c);
+    }
+  private:
+    // Declarar Variables de Estado
+    int fil, col, brujula;
+    estado actual, destino;
+    list<Action> plan;
+    //Nuevas variables de estado
+    Action ultimaAccion;
+    bool estoy_bien_situado, hayPlan;
+
+    // Métodos privados de la clase
+    bool pathFinding(int level, const estado &origen, const estado &destino, list<Action> &plan);
+    bool pathFinding_Profundidad(const estado &origen, const estado &destino, list<Action> &plan);
+    bool pathFinding_Anchura(const estado &origen, const estado &destino, list<Action> &plan);
+    bool pathFinding_CostoUniforme(const estado &origen, const estado &destino, list<Action> &plan);
+    void PintaPlan(list<Action> plan);
+    bool HayObstaculoDelante(estado &st);
+    
+
+};
+
+#endif
